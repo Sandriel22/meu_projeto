e6fb9d833624a2d2e73e783ddb1562efe288dbda
sandriel <sandrielsantos323@gmail.com>
2023-06-06 15:30:31 -0300

    novo views

--- meu_app/views.py
+++ meu_app/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'global/home.html')
 
 def sobre(request):
-    return render(request, 'sobre.html')
+    return render(request, 'global/sobre.html')
 
 def contato(request):
-    return render(request, 'contato.html')
+    return render(request, 'global/contato.html')
