c10afa703f083a44d69ac427929bb64345961239
Sandriel Soares <sandrielsantos323@gmail.com>
2023-06-22 11:45:01 -0300

    novo commit

--- meu_app/urls.py
+++ meu_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='base'),
     path('sobre/', views.sobre, name='sobre'),
     path('contato/', views.contato, name='contato'),
 ]
